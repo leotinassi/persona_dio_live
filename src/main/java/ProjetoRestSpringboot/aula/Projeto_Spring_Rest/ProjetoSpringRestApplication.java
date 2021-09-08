b3470896e6d0ac1e0d595966726f94c1184c3b43
leotinassi <68620678+leotinassi@users.noreply.github.com>
2021-09-08 16:44:00 -0300

    Comentarios
    Primeiro commit ao subir

---
+++ src/main/java/ProjetoRestSpringboot/aula/Projeto_Spring_Rest/ProjetoSpringRestApplication.java
@@ -0,0 +1,13 @@
+package ProjetoRestSpringboot.aula.Projeto_Spring_Rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProjetoRestSpringboot/aula/Projeto_Spring_Rest/ProjetoSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjetoRestSpringboot.aula.Projeto_Spring_Rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
